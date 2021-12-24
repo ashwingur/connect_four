2bf2c94b736fbacbefb829cd61a68062cd755ff8
Ashwin <ashwingur88@gmail.com>
2021-12-24 15:00:16 +1100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,6 @@ use connect_four::*;
 
 fn main() {
     let mut game_board = Board::new(Player::Red);
-
     game_board.run_game();
-    // game_board.update_cell(0, 3, Cell::Player(Player::Yellow));
-    // game_board.print();
 }
 
